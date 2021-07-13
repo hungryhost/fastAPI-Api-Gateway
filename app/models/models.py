18b2e0546a07849e6c883cadd733bdabc5c4f593
Юрий Бородин <yuiborodin@miem.hse.ru>
2021-07-14 00:18:15 +0300

    added auth flags to the user model

--- app/models/models.py
+++ app/models/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, Boolean
 
 from app.database import Base
 
@@ -7,10 +7,12 @@ class User(Base):
     __tablename__ = "users"
 
     id = Column(Integer, primary_key=True, index=True)
-    login = Column(String, unique=True, index=True, nullable=False)
-    name = Column(String)
-    email = Column(String)
+    email = Column(String, unique=True, index=True, nullable=False)
+    first_name = Column(String)
+    password = Column(String)
     picture = Column(String)
+    google_auth = Column(Boolean)
+    hse_auth = Column(Boolean)
 
     def get_display_name(self) -> str:
-        return self.name if self.name is not None else self.login
+        return self.first_name if self.first_name is not None else self.email
