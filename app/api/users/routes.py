962a8bbfc3de176cbb5fac6129ab5ef3852edcac
Юрий Бородин <yuiborodin@miem.hse.ru>
2021-07-16 06:16:09 +0300

    updated user endpoint

--- app/api/users/routes.py
+++ app/api/users/routes.py
@@ -1,21 +1,26 @@
 from fastapi import APIRouter, Depends, HTTPException
-from oauthlib.oauth2 import WebApplicationClient
 from sqlalchemy.orm import Session
+
+from app.api.auth.jwt_auth.dependencies import get_current_active_user
+from app.api.schemas import User
+from .response_models import Self
 from app.database import get_db
-from app.settings import settings
-from app.models.schemas import User
-from app.api.auth.jwt_lib.dependencies import get_user_from_header
-from app.models.models import User as DbUser
 from app.models.crud import get_user_by_id
+from app.models.models import User as DbUser
 
 router = APIRouter()
 
 
-@router.get("/me", response_model=User)
+@router.get("/self/", response_model=Self)
 def read_profile(
-		user: User = Depends(get_user_from_header),
-		db: Session = Depends(get_db), ) -> DbUser:
+		user: User = Depends(get_current_active_user),
+		db: Session = Depends(get_db), ) -> dict:
 	db_user = get_user_by_id(db, user.id)
+	return_dict = db_user.to_dict()
+	if db_user.password is None:
+		return_dict["has_password"] = False
+	else:
+		return_dict["has_password"] = True
 	if db_user is None:
 		raise HTTPException(status_code=404, detail="User not found")
-	return db_user
+	return return_dict
