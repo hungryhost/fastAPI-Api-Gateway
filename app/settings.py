c220475ff975dfe660db5ce50f1f5311cd060830
Юрий Бородин <yuiborodin@miem.hse.ru>
2021-07-16 06:15:16 +0300

    added jwt settings

--- app/settings.py
+++ app/settings.py
@@ -13,7 +13,9 @@ class Settings(BaseSettings):
     google_client_secret: str = Field("", env="GOOGLE_CLIENT_SECRET")
     jwt_secret_key: str = Field("example_key_super_secret", env="SECRET_KEY")
     jwt_algorithm: str = Field("HS256", env="ALGORITHM")
-    google_auth_path: str = Field("auth/google", env="GOOGLE_AUTH_CALLBACK_PATH")
+    jwt_access_expiry: int = Field(3600, env="JWT_ACCESS_EXPIRY")
+    jwt_refresh_expiry: int = Field(3600, env="JWT_REFRESH_EXPIRY")
+    google_auth_path: str = Field("api/v1/auth/external/google/", env="GOOGLE_AUTH_CALLBACK_PATH")
     google_conf_url: str = Field("https://accounts.google.com/.well-known/openid-configuration",
                                  env="GOOGLE_CONF_URL")
     google_login_url: str = Field("https://accounts.google.com/o/oauth2/v2/auth",
