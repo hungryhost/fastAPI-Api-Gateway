5b21a0a56b80d645fc8f594406d2228d7fffa9ee
Юрий Бородин <yuiborodin@miem.hse.ru>
2021-07-16 06:17:59 +0300

    added func to create user from registration

--- app/models/crud.py
+++ app/models/crud.py
@@ -4,7 +4,8 @@ from sqlalchemy.orm import Session
 
 
 from .models import User
-from .schemas import GoogleUser
+from app.api.schemas import GoogleUser
+from app.api.schemas import User as JwtUser
 
 
 def get_user_by_id(db: Session, user_id: int) -> Optional[User]:
@@ -26,6 +27,23 @@ def create_user_google(db: Session, google_user: GoogleUser) -> User:
         google_auth=True,
         hse_auth=False,
         picture=google_user.picture,
+    )
+    db.add(user)
+    db.commit()
+    db.refresh(user)
+
+    return user
+
+
+def create_user_jwt(db: Session, jwt_user: JwtUser) -> User:
+    user = User(
+        email=jwt_user.email,
+        first_name=jwt_user.first_name,
+        password=jwt_user.password,
+        last_name=jwt_user.last_name,
+        middle_name=jwt_user.middle_name,
+        google_auth=False,
+        hse_auth=False,
     )
     db.add(user)
     db.commit()
