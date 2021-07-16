bf76a6c9bed56f116268fee9d2620ef5218aa32e
Юрий Бородин <yuiborodin@miem.hse.ru>
2021-07-16 06:18:08 +0300

    added api router

---
+++ app/api/api_router.py
@@ -0,0 +1,9 @@
+from fastapi import APIRouter
+
+from api.auth.auth_router import auth_router
+from api.users.user_router import user_router
+
+api_router = APIRouter()
+
+api_router.include_router(user_router, tags=["users"])
+api_router.include_router(auth_router, tags=["auth"])
