069cf1f62fb323add40884d405bdca6ca53f4c37
Юрий Бородин <yuiborodin@miem.hse.ru>
2021-07-17 04:45:19 +0300

    changed hash alg to argon and added typing

--- app/api/auth/jwt_auth/password_authentication_backends.py
+++ app/api/auth/jwt_auth/password_authentication_backends.py
@@ -1,7 +1,11 @@
-from passlib.context import CryptContext
-from app.models.crud import get_user_by_email
+from typing import Union
 
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+from passlib.context import CryptContext
+from sqlalchemy.orm import Session
+
+from app.models.crud import get_user_by_email
+from app.schemas.core_schemas import User as UserSchema
+pwd_context = CryptContext(schemes=["argon2"], deprecated="auto")
 
 
 def verify_password(plain_password: str, hashed_password: str) -> CryptContext:
@@ -12,10 +16,10 @@ def get_password_hash(password: str) -> CryptContext:
     return pwd_context.hash(password)
 
 
-def authenticate_user(fake_db, email: str, password: str):
-    user = get_user_by_email(fake_db, email)
+def authenticate_user(db: Session, email: str, password: str) -> Union[bool, UserSchema]:
+    user = get_user_by_email(db, email)
     if not user:
         return False
-    if not verify_password(password, user.password):
+    if not verify_password(plain_password=password, hashed_password=user.password):
         return False
-    return user
+    return UserSchema(**user.to_dict())
