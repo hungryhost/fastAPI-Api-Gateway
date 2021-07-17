b5533c42a63f271ba83b467857999a134b297d27
Юрий Бородин <yuiborodin@miem.hse.ru>
2021-07-17 04:47:16 +0300

    added registration endpoint

--- app/api/auth/jwt_auth/routes.py
+++ app/api/auth/jwt_auth/routes.py
@@ -1,7 +1,9 @@
+from typing import Union
+
 from fastapi import APIRouter, Depends, status, HTTPException
 from sqlalchemy.orm import Session
 
-from app.api.auth.jwt_auth.jwt_utils import create_access_token
+from app.api.auth.jwt_auth.jwt_utils import create_access_token, create_refresh_token
 from app.database import get_db
 from .password_authentication_backends import authenticate_user
 from app.schemas.auth_schemas import RegistrationModel, LoginModel, RegistrationResponseModel, LoginResponseModel
@@ -9,13 +11,33 @@ from .registration_backends import create_user
 router = APIRouter()
 
 
-@router.post('/auth/internal/register/')
-async def register(registration_model: RegistrationModel):
-	return registration_model
+@router.post('/auth/internal/register/', response_model=RegistrationResponseModel)
+async def register(
+		registration_model: RegistrationModel,
+		db: Session = Depends(get_db)) -> Union[HTTPException, dict]:
+	user = create_user(db=db, reg_info=registration_model)
+	if not user:
+		raise HTTPException(
+			status_code=status.HTTP_400_BAD_REQUEST,
+			detail="User already exists.",
+		)
+	access_token = create_access_token(
+		data={"sub": user.email}
+	)
+	refresh_token = create_refresh_token(
+		data={"sub": user.email}
+	)
+	return {
+		"access_token": access_token,
+		"refresh_token": refresh_token,
+		"user_info": user.dict(exclude={'password'})
+	}
 
 
-@router.post('/auth/internal/login/')
-async def login(login_model: LoginModel, db: Session = Depends(get_db)):
+@router.post('/auth/internal/login/', response_model=LoginResponseModel)
+async def login(
+		login_model: LoginModel,
+		db: Session = Depends(get_db)) -> Union[HTTPException, dict]:
 	user = authenticate_user(db, login_model.email, login_model.password)
 	if not user:
 		raise HTTPException(
@@ -26,4 +48,11 @@ async def login(login_model: LoginModel, db: Session = Depends(get_db)):
 	access_token = create_access_token(
 		data={"sub": user.email}
 	)
-	return {"access_token": access_token, "token_type": "bearer"}
+	refresh_token = create_refresh_token(
+		data={"sub": user.email}
+	)
+	return {
+		"access_token": access_token,
+		"refresh_token": refresh_token,
+		"user_info": user.dict(exclude={'password'})
+	}
