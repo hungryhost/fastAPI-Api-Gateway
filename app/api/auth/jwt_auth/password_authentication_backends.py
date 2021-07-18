094bd8ead5f0fc2fe5a413864576c9be0e3f1060
Юрий Бородин <yuiborodin@miem.hse.ru>
2021-07-18 05:04:31 +0300

    changed types

--- app/api/auth/jwt_auth/password_authentication_backends.py
+++ app/api/auth/jwt_auth/password_authentication_backends.py
@@ -1,10 +1,9 @@
 from typing import Union
-
 from passlib.context import CryptContext
 from sqlalchemy.orm import Session
-
+from app.models.models import UserModel
 from app.models.crud import get_user_by_email
-from app.schemas.core_schemas import User as UserSchema
+from app.schemas.core_schemas import UserCoreSchema
 pwd_context = CryptContext(schemes=["argon2"], deprecated="auto")
 
 
@@ -16,10 +15,10 @@ def get_password_hash(password: str) -> CryptContext:
     return pwd_context.hash(password)
 
 
-def authenticate_user(db: Session, email: str, password: str) -> Union[bool, UserSchema]:
-    user = get_user_by_email(db, email)
+def authenticate_user(db: Session, email: str, password: str) -> Union[bool, UserCoreSchema]:
+    user: UserModel = get_user_by_email(db, email)
     if not user:
         return False
     if not verify_password(plain_password=password, hashed_password=user.password):
         return False
-    return UserSchema(**user.to_dict())
+    return UserCoreSchema(**user.to_dict())
