7dfcd1865aecc4fb098e742a85b5075e83634013
Юрий Бородин <yuiborodin@miem.hse.ru>
2021-07-18 05:04:38 +0300

    changed types

--- app/api/auth/jwt_auth/registration_backends.py
+++ app/api/auth/jwt_auth/registration_backends.py
@@ -2,17 +2,20 @@ from typing import Union
 
 from sqlalchemy.orm import Session
 from app.models.crud import get_user_by_email, create_user_jwt
+from app.models.models import UserModel
 from auth_schemas import RegistrationModel
 from .password_authentication_backends import get_password_hash
 from app.schemas.core_schemas import User as UserSchema
+from app.schemas.core_schemas import UserCoreSchema
 
 
-def create_user(db: Session, reg_info: RegistrationModel) -> Union[bool, UserSchema]:
-	user = get_user_by_email(db, reg_info.email)
+def create_user(db: Session, reg_info: RegistrationModel) -> Union[bool, UserCoreSchema]:
+	user: UserModel = get_user_by_email(db, reg_info.email)
 	if user:
 		return False
-	jwt_user = UserSchema(
+
+	jwt_user: UserSchema = UserSchema(
 		**reg_info.dict(exclude={'password', "password_confirmation"}),
 		password=get_password_hash(reg_info.password))
-	created_user = create_user_jwt(db=db, jwt_user=jwt_user)
-	return UserSchema(**created_user.to_dict())
+	created_user: UserModel = create_user_jwt(db=db, jwt_user=jwt_user)
+	return UserCoreSchema(**created_user.to_dict())
