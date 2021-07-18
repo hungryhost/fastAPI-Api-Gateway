9659d505fef504fb04f1083e5ac2ab81b29b8764
Юрий Бородин <yuiborodin@miem.hse.ru>
2021-07-18 05:07:41 +0300

    added prefixes for endpoints

--- app/api/api_router.py
+++ app/api/api_router.py
@@ -5,5 +5,5 @@ from api.users.user_router import user_router
 
 api_router = APIRouter()
 
-api_router.include_router(user_router)
-api_router.include_router(auth_router)
+api_router.include_router(user_router, prefix='/users')
+api_router.include_router(auth_router, prefix='/auth')
