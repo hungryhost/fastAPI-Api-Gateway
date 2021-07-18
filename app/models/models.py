77e2e9b021b0c0a1b401fd297d7d8cb6eb84e66c
Юрий Бородин <yuiborodin@miem.hse.ru>
2021-07-18 05:04:56 +0300

    renamed User model to UserModel for clarification

--- app/models/models.py
+++ app/models/models.py
@@ -3,7 +3,7 @@ from sqlalchemy import Column, Integer, String, Boolean
 from app.database import Base
 
 
-class User(Base):
+class UserModel(Base):
     __tablename__ = "users"
 
     id = Column(Integer, primary_key=True, index=True)
