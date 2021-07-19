8e6daa5e72fd908406d8c88405b712fca6f48b8f
Юрий Бородин <yuiborodin@miem.hse.ru>
2021-07-19 09:41:29 +0300

    fixed bug with docker

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
-from api.api_router import api_router
+from app.api.api_router import api_router
 app = FastAPI()
 #app.add_middleware(SessionMiddleware, secret_key="!secret")
 app.add_middleware(
