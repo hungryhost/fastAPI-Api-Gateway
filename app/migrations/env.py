3f6c6fb418784f87e7b1de09de6ed01f255d9b56
Юрий Бородин <yuiborodin@miem.hse.ru>
2021-07-19 08:36:55 +0300

    fixed bug with alembic

--- app/migrations/env.py
+++ app/migrations/env.py
@@ -6,13 +6,14 @@ from sqlalchemy import pool
 from alembic import context
 
 
-from app.settings import settings
-from app.database import Base
-from app.models.models import *
+
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
 sys.path = ["", ".."] + sys.path[1:]
+from app.settings import settings
+from app.database import Base
+from app.models.models import *
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
 fileConfig(config.config_file_name)
