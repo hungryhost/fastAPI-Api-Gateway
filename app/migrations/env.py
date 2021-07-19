77f12b559e5792c5eac029ada6038d902d46adb2
Юрий Бородин <yuiborodin@miem.hse.ru>
2021-07-19 08:32:33 +0300

    moved db related files

--- app/migrations/env.py
+++ app/migrations/env.py
@@ -4,7 +4,7 @@ from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 
 from alembic import context
-sys.path = ["", ".."] + sys.path[1:]
+
 
 from app.settings import settings
 from app.database import Base
@@ -12,7 +12,7 @@ from app.models.models import *
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
-
+sys.path = ["", ".."] + sys.path[1:]
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
 fileConfig(config.config_file_name)
