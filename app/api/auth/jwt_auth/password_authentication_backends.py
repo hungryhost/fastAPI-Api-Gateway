612180e2de204dd4b375d7e29824af9e9b80c3fc
hungryhost <hungryhost@icloud.com>
2021-07-20 03:55:08 +0300

    Update password_authentication_backends.py

--- app/api/auth/jwt_auth/password_authentication_backends.py
+++ app/api/auth/jwt_auth/password_authentication_backends.py
@@ -4,6 +4,11 @@ from sqlalchemy.orm import Session
 from models.db_models import UserModel
 from models.crud import get_user_by_email
 from schemas.core_schemas import UserCoreSchema
+import logging
+logging.basicConfig(level=logging.INFO)
+name_logger = logging.getLogger(__name__)
+name_logger.setLevel(logging.INFO)
+
 pwd_context = CryptContext(schemes=["argon2"], deprecated="auto")
 
 
@@ -16,9 +21,15 @@ def get_password_hash(password: str) -> CryptContext:
 
 
 def authenticate_user(db: Session, email: str, password: str) -> Union[bool, UserCoreSchema]:
+    start = time.time()
     user: UserModel = get_user_by_email(db, email)
+    end = time.time()
+    name_logger.warning("got user in {}".format(end - start))
     if not user:
         return False
+    start = time.time()
     if not verify_password(plain_password=password, hashed_password=user.password):
         return False
+    end = time.time()
+    name_logger.warning("verified pass in {}".format(end - start))
     return UserCoreSchema(**user.to_dict())
