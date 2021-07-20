87dc01f3cd01ee6120d24fc9f3597ff940a8c120
hungryhost <hungryhost@icloud.com>
2021-07-20 04:00:49 +0300

    Update password_authentication_backends.py

--- app/api/auth/jwt_auth/password_authentication_backends.py
+++ app/api/auth/jwt_auth/password_authentication_backends.py
@@ -11,7 +11,7 @@ logging.basicConfig(level=logging.INFO)
 name_logger = logging.getLogger(__name__)
 name_logger.setLevel(logging.INFO)
 
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+pwd_context = CryptContext(schemes=["argon2"], deprecated="auto")
 
 
 def verify_password(plain_password: str, hashed_password: str) -> CryptContext:
